bb36a7e58ae5425c7c6f683f194ddca0b4bd5f6b
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-09-30 14:31:43 -0700

    Working add feature

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // import functions and grab DOM elements
+import { add } from './calculations.js';
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-btn');
+const addOutput = document.getElementById('add-output');
 
 // initialize global state
 
@@ -6,3 +11,10 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+addButton.addEventListener('click', () => {
+    const value1 = Number(addInput1.value);
+    const value2 = Number(addInput2.value);
+    const result = add(value1, value2);
+    addOutput.textContent = result;
+});
