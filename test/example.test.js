757831a36a40b4deb3533079a073d60c5bb14c72
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-09-30 13:49:22 -0700

    Makes functioning test

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { add } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -15,4 +16,31 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+
+test('add function', (expect) => {
+    //Arrange
+    const input1 = 3;
+    const input2 = 2;
+    const expected = 5;
+
+    //Act
+    const actual = add(input1, input2);
+
+    // Assert
+    expect.equal(actual, expected);
+});
+
+test('add function', (expect) => {
+    //Arrange
+    const input1 = 4;
+    const input2 = 5;
+    const expected = 9;
+
+    //Act
+    const actual = add(input1, input2);
+
+    // Assert
+    expect.equal(actual, expected, 'should add 4 and 5');
 });
